450bc45f16547a8d2f02692a98548be8131ba635
Yaping Li <yapingbupt@gmail.com>
2017-06-20 11:42:00 -0400

    Add post and patch actions that allow to create and edit a movie

--- Rails_Flix/config/routes.rb
+++ Rails_Flix/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
     root "movies#index"
-    get "/movies" => "movies#index"
-    get "/movies/:id" => "movies#show", as: 'movie'
+    resources :movies
+    
+    # get "/movies" => "movies#index"
+    # post "/movies" => "movies#create"
+    # get "/movies/new" => "movies#new", as: 'new_movie'
+    # get "/movies/:id" => "movies#show", as: 'movie'
+    # get "/movies/:id/edit" => "movies#edit", as: 'edit_movie'
+    # patch "/movies/:id" => "movies#update"  #In Rails, post verb is used to create, patch verb is used to update
 end
